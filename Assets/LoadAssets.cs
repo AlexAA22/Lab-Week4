c19244e6daed147ade3635e30ffaf3b4fb64cbe5
AlexAA22 <alex.chen-2@student.uts.edu.au>
2022-08-22 17:50:24 +1000

    unity project
    Second commit.(70)

---
+++ Assets/LoadAssets.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+   
+ 
+    public class LoadAssets : MonoBehaviour
+         
+    {
+        public GameObject redObj;
+        [SerializeField]
+        private GameObject blueObj;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        // Vector3(2.0f, 0.0f, 0.0f);
+        Instantiate(redObj, new Vector3(2.0f, 0.0f, 0.0f), Quaternion.identity);
+        Instantiate(blueObj, new Vector3(-2.0f, 0.0f, 0.0f), Quaternion.identity);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
